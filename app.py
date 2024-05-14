0f64ef6da53121023c6befda06dea05eb07675eb
kennethscff <kenneth@kenneth.co.uk>
2024-05-14 21:46:47 +0100

    Version 1

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello, {name}!"
+
+@app.route('/sum', methods=['POST'])
+def sum_numbers():
+    data = request.json
+    result = sum(data['numbers'])
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run()
